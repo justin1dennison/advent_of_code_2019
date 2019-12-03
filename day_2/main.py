a187ad615480322fda1ed7cd09a070c461bc9471
Justin Dennison <justin1dennison@gmail.com>
2019-12-03 10:34:11 -0500

    completed part two of day two

--- day_2/main.py
+++ day_2/main.py
@@ -1,6 +1,8 @@
-def main():
-    with open("./input.txt") as f:
-        state = [int(el) for el in f.read().split(",")]
+from itertools import product
+
+def step(state, noun, verb):
+    state[1] = noun
+    state[2] = verb
     chunks = [state[i : i + 4] for i in range(0, len(state) - 1, 4)]
     for chunk in chunks:
         opcode, x, y, out = chunk
@@ -13,9 +15,19 @@ def main():
             result = state[x] * state[y]
             state[out] = result
         else:
-            raise ValueError("Unknown Opcode")
-    print(f"State: {state}")
+            raise ValueError('Unknown Opcode')
+    return state
 
+def main():
+    with open("./input.txt") as f:
+        original_state = [int(el) for el in f.read().split(",")]
+    nouns = range(100)
+    verbs = range(100)
+    for noun, verb in product(nouns, verbs):
+        goal, *_ = step(original_state[:], noun, verb)
+        if goal == 19690720:
+            print(f'Result: {100 * noun + verb}')
+            break
 
 if __name__ == "__main__":
     main()
